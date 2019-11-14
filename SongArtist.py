bd125a2d330545a9f09df5a954962ed3b10c0693
dngheelee <54331233+heeSchae@users.noreply.github.com>
2019-11-14 10:27:49 +0100

    Update SongArtist.py

--- SongArtist.py
+++ SongArtist.py
@@ -35,6 +35,7 @@
 #When your code is done running, there should exist three
 #variables: song_1, song_2, and song_3, according to the
 #requirements above.
-song_1 = Song("You Belong With Me", "Fearless", 2008, Artist("Taylor Swift", "Big Machine Records, LLC"))
-song_2 = Song("All Too Well", "Red", 2012, Artist("Taylor Swift", "Big Machine Records, LLC"))
+TS = Artist("Taylor Swift", "Big Machine Records, LLC")
+song_1 = Song("You Belong With Me", "Fearless", 2008, TS)
+song_2 = Song("All Too Well", "Red", 2012, TS)
 song_3 = Song("Up We Go", "Midnight Machines", 2016, Artist("LiGHTS", "Warner Bros. Records Inc."))
