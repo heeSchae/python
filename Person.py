7a84e65bb6d9e8a7b654dda561cad4ebdc17663c
dngheelee <54331233+heeSchae@users.noreply.github.com>
2019-11-14 10:12:44 +0100

    Create Person.py

---
+++ Person.py
@@ -0,0 +1,28 @@
+#Classes can also have references to other instances of
+#themselves. Consider this Person class, for example, 
+#that allows for an instance of a father and mother
+#to be given in the constructor.
+#
+#Create 3 instances of this class. The first should have 
+#the name "Mr. Burdell" with an age of 53. The second
+#instance should have a name of "Mrs. Burdell" with an age
+#of 53 as well. Finally, make an instance with the name of
+#"George P. Burdell" with an age of 25. This final instance
+#should also have the father attribute set to the instance 
+#of Mr. Burdell, and the mother attribute set to the 
+#instance of Mrs. Burdell. Finally, store the instance of 
+#George P. Burdell in a variable called george_p. (It does
+#not matter what variable names you use for Mr. and Mrs.
+#Burdell.)
+
+class Person:
+    def __init__(self, name, age, father=None, mother=None):
+        self.name = name
+        self.age = age
+        self.father = father
+        self.mother = mother
+george_p = Person("George P. Burdell", 25)
+the_mother = Person("Mrs. Burdell", 53)
+the_father = Person("Mr. Burdell", 53)
+george_p.mother = the_mother
+george_p.father = the_father
