9b3e4c2a463d78c36f5ca3a6fdbe7e272a1c9e5c
dngheelee <54331233+heeSchae@users.noreply.github.com>
2019-11-21 14:48:15 +0100

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -1,27 +1,10 @@
-#Write a function called linear() that takes two parameters
-#- a list of strings and a string. Write this function so
-#that it returns the first index at which the string is
-#found within the list if the string is found, or False if
-#it is not found. You do not need to worry about searching
-#for the search string inside the individual strings within
-#the list: for example, linear(["bobby", "fred"], "bob")
-#should return False, but linear(["bob", "fred"], "bob")
-#should return 0.
-#
-#Use a linear search algorithm (not as scary as it sounds).
-#Do not use the list method index -- in this exercise,
-#you're actually implementing the way the index method
-#works!
-
-
-#Write your code here!
 def linear(a_list, string):
     result = ""
     for i in range(len(a_list)):
         if string == a_list[i]:
             result = i
             return result 
-    if result = "":
+    if result == "":
         return False
     
     
@@ -34,3 +17,4 @@ def linear(a_list, string):
 #print: 3
 a_list = [5, 1, 3, 6, 7, 3, 1, 6, 7, 8, 3, 6]
 print(linear(a_list, 6))
+
